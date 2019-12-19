b85ad909e5a2aadddc16321117ac125230309607
Doron Bali <doronb@outlook.com>
2019-12-19 10:46:43 +0200

    create lambda for deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 
-let data = {
-  balance: -369
-}
 
 function App() {
-  const [balance, setBalance] = useState(data.balance);
+  const [balance, setBalance] = useState({balance: 255});
+  useEffect(() => {
+    async function fetchData () {
+      const result = await axios(
+        'https://axuw2l7p16.execute-api.us-east-2.amazonaws.com/dev',
+      );
+      console.log("result", result);
+      setBalance(result.data);
+    };
+    fetchData();
+  }, []);
   // setInterval(()=>{
   //   setBalance(balance + 1);
   //   console.log(balance);
@@ -18,7 +26,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          your balance is { balance }
+          your balance is { balance.balance }
         </p>
         <a
           className="App-link"
